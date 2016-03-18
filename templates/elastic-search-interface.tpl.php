4b424c4a58b13f69532bf667eef758729da90f6b
Ming Chen <ming.chen0919@gmail.com>
2016-03-18 12:09:59 -0400

    custom theme and template for elastic_search_interface_form

---
+++ templates/elastic-search-interface.tpl.php
@@ -0,0 +1,13 @@
+
+
+<ul>
+	<li><?php print $uniquename; ?></li>
+	<li><?php print $organism; ?></li>
+	<li><?php print $seqtype; ?></li>
+	<li><?php print $seqlen; ?></li>
+	<li><?php print $blast_hit_description; ?></li>
+	<li><?php print $blast_hit_score; ?></li>
+	<li><?php print $blast_hit_eval; ?></li>
+</ul>
+
+
