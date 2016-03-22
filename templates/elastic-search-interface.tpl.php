33ba2a62fc76310421b5ab7b28a2446ea6746ff0
Ming Chen <ming.chen0919@gmail.com>
2016-03-22 12:04:17 -0400

    moving around code chunks to make it more organized

--- templates/elastic-search-interface.tpl.php
+++ templates/elastic-search-interface.tpl.php
@@ -1,4 +1,19 @@
-	<div ><?php print $children; ?></div>
+<?php dpm($variables); ?>
+
+	<h3>Elasticsearch: transcripts</h3>
+	<div style="padding:10px"><?php print $organism; ?></div>
+	<div style="padding:10px"><?php print $uniquename; ?></div>
+	<div style="padding:10px"><?php print $seqtype; ?></div>
+	<div style="padding:10px"><?php print $blast_hit_description; ?></div>
+	<div><?php print $search_transcripts_submit; ?></div>
+	
+	<br/>
+	<br/>
+	<h3>Elasticsearch: webpages</h3>
+	<div style="padding:10px"><?php print $search_webpages; ?></div>
+	<div><?php print $search_webpages_submit; ?></div>
+
+	<div><?php print $children; ?></div>
 
 
 	<div><br/><br/><br/></div>
