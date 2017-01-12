07b1613ff481831e1e7e1a847364b5f338195261
Ming Chen <ming.chen0919@gmail.com>
2017-01-12 17:54:04 +0000

    check indices json template files

--- test-scripts.php
+++ test-scripts.php
@@ -2,6 +2,23 @@
 
 require_once drupal_get_path('module', 'tripal_elasticsearch').'/vendor/autoload.php';
 
-$client = Elasticsearch\ClientBuilder::create()->setHosts(variable_get('elasticsearch_hosts', array()))->build();
+$files = scandir(drupal_get_path('module', 'tripal_elasticsearch').'/elasticsearch_indices_json_templates', SCANDIR_SORT_DESCENDING);
+foreach($files as $key=>$value) {
+  if(!preg_match('/\.json$/', $value)) {
+    unset($files[$key]);
+  }
+}
 
-var_dump($client->health());
+var_dump($files);
+
+/*
+var_dump(variable_get('elasticsearch_hots'));
+$client = Elasticsearch\ClientBuilder::create()->setHosts(variable_get('elasticsearch_hosts', array('127.0.0.1:9201')))->build();
+
+
+
+$elasticsearch_index_json = json_decode(file_get_contents(drupal_get_path('module', 'tripal_elasticsearch').'/elasticsearch_index.json'), true);
+var_dump($elasticsearch_index_json);
+$response = $client->indices()->create($elasticsearch_index_json);
+var_dump($response);
+*/
