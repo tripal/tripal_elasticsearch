ca3840869e12959cde3f80c3f2ebd05fd2da8754
Ming Chen <ming.chen0919@gmail.com>
2017-01-15 17:26:38 +0000

    added form validation

--- test-scripts.php
+++ test-scripts.php
@@ -1,6 +1,15 @@
 <?php
 
 require_once drupal_get_path('module', 'tripal_elasticsearch').'/vendor/autoload.php';
+
+  $sql = "SELECT nid FROM node WHERE status=1 ORDER BY nid LIMIT 1;";
+  $result = db_query($sql)->fetchAll();
+  foreach($result as $record) {
+    var_dump($record->nid);
+  }
+
+
+/*
     $sql_table_list = "SELECT table_name FROM information_schema.tables WHERE (table_schema = 'public' OR table_schema = 'chado') ORDER BY table_name;";
     $sql_public_tables = "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public' ORDER BY table_name;";
     $public_tables = db_query($sql_public_tables);
@@ -8,7 +17,9 @@ require_once drupal_get_path('module', 'tripal_elasticsearch').'/vendor/autoload
       $table = $record->table_name;
       $public_tables[$table] = $table;
     } 
-/*
+
+
+
 $files = scandir(drupal_get_path('module', 'tripal_elasticsearch').'/elasticsearch_indices_json_templates', SCANDIR_SORT_DESCENDING);
 foreach($files as $key=>$value) {
   if(!preg_match('/\.json$/', $value)) {
