baef3af0b0ceebe15e3ad742e3d0e5d08a1be1d5
mchen33 <ming.chen0919@gmail.com>
2017-01-28 09:28:13 -0500

    removed test-scripts.php

--- test-scripts.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once drupal_get_path('module', 'tripal_elasticsearch').'/vendor/autoload.php';
-
-$client = Elasticsearch\ClientBuilder::create()->setHosts(variable_get('elasticsearch_hosts', array()))->build();
-
-var_dump($client->health());
