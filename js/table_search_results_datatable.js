38f3de2c9f1ee4c7be1d938d9768c28297eb7d3b
MChen <ming.chen0919@gmail.com>
2017-06-02 13:48:39 -0400

    updated table search interface

--- js/table_search_results_datatable.js
+++ js/table_search_results_datatable.js
@@ -1,10 +1,15 @@
 (function ($) {
     Drupal.behaviors = Drupal.behaviors || {};
 
-    Drupal.behaviors.indices_delete_confirm = {
+    Drupal.behaviors.datatable_search_results = {
         attach: function (context, settings) {
             $(document).ready(function () {
-                $('#table_search_results_datatable .sticky-enabled').DataTable();
+                $('#table_search_results_datatable .sticky-enabled').DataTable({
+                    language:{
+                        search: 'Filter current results'
+                    },
+                    retrieve: true,
+                });
             })
         }
     }
