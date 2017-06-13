d18b1584c81c5149aa76ea1e82a824abd7848115
Abdullah Almsaeed <almasaeed2010@gmail.com>
2017-06-13 14:05:58 -0400

    Tests

--- js/table_search_results_datatable.js
+++ js/table_search_results_datatable.js
@@ -1,23 +1,15 @@
 (function ($) {
-    Drupal.behaviors = Drupal.behaviors || {};
-
-    Drupal.behaviors.datatable_search_results = {
-        attach: function (context, settings) {
-            $(document).ready(function () {
-                // var table = $('#table_search_results_datatable .sticky-enabled').DataTable({
-                //     language: {
-                //         search: 'Filter current results'
-                //     },
-                //     retrieve: true
-                // });
-                //
-                // table.on('page.dt', function () {
-                //     var info = table.page.info();
-                //     // Total number of pages = info.pages
-                //     // Current page = info.page
-                //     //$('#pageInfo').html('Showing page: ' + info.page + ' of ' + info.pages);
-                // });
-            });
-        }
-    };
+  Drupal.behaviors = Drupal.behaviors || {};
+  Drupal.behaviors.datatable_search_results = {
+    attach: function (context, settings) {
+      $(document).ready(function () {
+        // var table = $('#table_search_results_datatable
+        // .sticky-enabled').DataTable({ language: { search: 'Filter current
+        // results' }, retrieve: true });  table.on('page.dt', function () {
+        // var info = table.page.info(); // Total number of pages = info.pages
+        // // Current page = info.page //$('#pageInfo').html('Showing page: ' +
+        // info.page + ' of ' + info.pages); });
+      });
+    }
+  };
 })(jQuery);
