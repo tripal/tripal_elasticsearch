d7f5d04f28d3cebfce8fbc5d1c263805d940d5df
MChen <ming.chen0919@gmail.com>
2017-06-13 13:55:53 -0400

    drupal pagination

--- js/table_search_results_datatable.js
+++ js/table_search_results_datatable.js
@@ -4,19 +4,19 @@
     Drupal.behaviors.datatable_search_results = {
         attach: function (context, settings) {
             $(document).ready(function () {
-                var table = $('#table_search_results_datatable .sticky-enabled').DataTable({
-                    language: {
-                        search: 'Filter current results'
-                    },
-                    retrieve: true
-                });
-
-                table.on('page.dt', function () {
-                    var info = table.page.info();
-                    // Total number of pages = info.pages
-                    // Current page = info.page
-                    //$('#pageInfo').html('Showing page: ' + info.page + ' of ' + info.pages);
-                });
+                // var table = $('#table_search_results_datatable .sticky-enabled').DataTable({
+                //     language: {
+                //         search: 'Filter current results'
+                //     },
+                //     retrieve: true
+                // });
+                //
+                // table.on('page.dt', function () {
+                //     var info = table.page.info();
+                //     // Total number of pages = info.pages
+                //     // Current page = info.page
+                //     //$('#pageInfo').html('Showing page: ' + info.page + ' of ' + info.pages);
+                // });
             });
         }
     };
