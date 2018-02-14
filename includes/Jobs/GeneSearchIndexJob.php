e7fd059dfd877e3805268e29ee99fba746491588
Abdullah Almsaeed <almasaeed2010@gmail.com>
2018-02-14 08:51:56 -0500

    Clean up code

--- includes/Jobs/GeneSearchIndexJob.php
+++ includes/Jobs/GeneSearchIndexJob.php
@@ -276,19 +276,16 @@ class GeneSearchIndexJob extends ESJob {
    * @return int
    */
   public function total() {
+    $sql = 'SELECT COUNT(*) FROM {chado_feature} 
+              ORDER BY nid ASC 
+              OFFSET :offset LIMIT :limit';
+
     if ($this->tripal_version === 3) {
       $sql = 'SELECT COUNT(*) FROM ' . db_escape_table($this->bundle_table) . ' 
                 ORDER BY mapping_id ASC
                 OFFSET :offset LIMIT :limit';
-      return db_query($sql, [
-        ':offset' => $this->offset,
-        ':limit' => $this->limit,
-      ])->fetchField();
     }
 
-    $sql = 'SELECT COUNT(*) FROM {chado_feature} 
-              ORDER BY nid ASC 
-              OFFSET :offset LIMIT :limit';
     return db_query($sql, [
       ':offset' => $this->offset,
       ':limit' => $this->limit,
