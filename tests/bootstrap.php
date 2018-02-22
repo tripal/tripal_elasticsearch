e8d9e108ef26f047f083ac3dc5f8c569e88323c6
Abdullah Almsaeed <almasaeed2010@gmail.com>
2018-02-22 17:12:30 -0500

    Add correct env variables

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,8 @@
 <?php
-test_suite_read_and_set_environment();
+test_suite_read_and_set_environment_variables();
 
 $drupal_root = getenv('DRUPAL_ROOT');
-define('DRUPAL_ROOT', $drupal_root ? $drupal_root : __DIR__.'/../../../../..');
+define('DRUPAL_ROOT', $drupal_root ?: __DIR__.'/../../../../..');
 require_once DRUPAL_ROOT.'/includes/bootstrap.inc';
 $_SERVER['REMOTE_ADDR'] = '127.0.0.1';
 
@@ -16,11 +16,11 @@ chdir($current_dir);
 /**
  * @throws \Exception
  */
-function test_suite_read_and_set_environment() {
-    $filename = __DIR__.'/../.env';
+function test_suite_read_and_set_environment_variables() {
+    $filename = __DIR__.'/.env';
     if(file_exists($filename)) {
         $file = fopen($filename, 'r');
-        while ($line = readline($file)) {
+        while ($line = str_replace("\n", '', fgets($file))) {
             // break line into key value
             $env = explode('=', $line);
             if(count($env) === 2) {
