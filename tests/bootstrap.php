20fb2eb9d2ba45660da4372c505dec15047db251
Abdullah Almsaeed <almasaeed2010@gmail.com>
2018-02-23 15:55:22 -0500

    Try once more

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,11 +3,12 @@ test_suite_read_and_set_environment_variables();
 
 $drupal_root = getenv('DRUPAL_ROOT');
 define('DRUPAL_ROOT', $drupal_root ?: __DIR__.'/../../../../..');
-require_once DRUPAL_ROOT.'/includes/bootstrap.inc';
-$_SERVER['REMOTE_ADDR'] = '127.0.0.1';
 
 $current_dir = getcwd();
 chdir(DRUPAL_ROOT);
+
+require_once DRUPAL_ROOT.'/includes/bootstrap.inc';
+$_SERVER['REMOTE_ADDR'] = '127.0.0.1';
 
 // Bootstrap Drupal.
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
