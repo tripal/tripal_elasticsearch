de82e5160b460b5625f06cc816fadfc8606de9c1
Abdullah Almsaeed <almasaeed2010@gmail.com>
2019-02-20 12:37:00 -0500

    Create tests for the Clause class

---
+++ tests/Feature/ClauseTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature;
+
+use ES\Query\Clause;
+use Tests\TestCase;
+
+class ClauseTest extends TestCase{
+
+  /** @test */
+  public function testThatWeCanBuildWhereQueriesWithoutUsingClosures() {
+    $clause = new Clause();
+    $query = $clause->where('field', 'value')->where('value')->orWhere('value');
+    $this->assertEquals('field:value AND value OR value', $query->build());
+  }
+
+  /** @test */
+  public function testThatClosuresGenerateEnclosedParameters() {
+    $clause = new Clause();
+    $query = $clause->where(function(Clause $query) {
+      $query->where('f', 'v');
+      $query->where('f', 'v');
+    })->orWhere(function (Clause $query) {
+      $query->where('f', 'v');
+      $query->orWhere('v');
+    })->build();
+
+    $this->assertEquals('(f:v AND f:v) OR (f:v OR v)', $query);
+  }
+}
